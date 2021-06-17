d565843d1a9f6f12bbbd21612137fc0a86dfc972
Michael Easter <codetojoy@gmail.com>
2021-06-17 08:20:06 -0300

    try catch

--- src/main/groovy/net/codetojoy/waro/Main.groovy
+++ src/main/groovy/net/codetojoy/waro/Main.groovy
@@ -1,14 +1,18 @@
 
 package net.codetojoy.waro
 
-import net.codetojoy.waro.casino.Tourney 
+import net.codetojoy.waro.casino.Tourney
 
 // TODO: add logging
 
-def config = new Config(args[0])
+try {
+    def config = new Config(args[0])
 
-def tourney = new Tourney(config)
-tourney.playGames()
+    def tourney = new Tourney(config)
+    tourney.playGames()
+} catch (Exception e) {
+   System.err.println "internal error: $e.message"
+}
 
 
 
