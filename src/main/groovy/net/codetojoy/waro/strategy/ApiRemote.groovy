6c714d8ae34f59e5c633a4b4b53dbe5bc9528b02
Michael Easter <codetojoy@gmail.com>
2021-06-19 11:36:46 -0300

    groovier

--- src/main/groovy/net/codetojoy/waro/strategy/ApiRemote.groovy
+++ src/main/groovy/net/codetojoy/waro/strategy/ApiRemote.groovy
@@ -2,11 +2,7 @@ package net.codetojoy.waro.strategy
 
 import net.codetojoy.waro.strategy.api.*
 
-import org.apache.http.HttpEntity
-import org.apache.http.HttpHeaders
-import org.apache.http.client.methods.CloseableHttpResponse
 import org.apache.http.client.methods.HttpGet
-import org.apache.http.impl.client.CloseableHttpClient
 import org.apache.http.impl.client.HttpClients
 import org.apache.http.util.EntityUtils
 
@@ -16,18 +12,18 @@ import groovy.transform.NullCheck
 class ApiRemote implements Strategy {
     def apiURI
 
-    public ApiRemote(String scheme, String host, String path, String mode) {
+    ApiRemote(String scheme, String host, String path, String mode) {
         this.apiURI = new ApiURI(scheme, host, path, mode)
     }
 
     @Override
-    public int selectCard(int prizeCard, List<Integer> hand, int maxCard) {
+    int selectCard(int prizeCard, List<Integer> hand, int maxCard) {
         var bid = 0
 
         try {
              bid = apiRemoteSelectCard(prizeCard, hand, maxCard)
         } catch (Exception ex) {
-            System.err.println("ERROR caught exception: " + ex.getMessage())
+            System.err.println("ERROR caught exception: ${ex.message}")
             // bail out for now
             System.exit(-1)
         }
@@ -44,15 +40,15 @@ class ApiRemote implements Strategy {
         var card = 0
         var uri = apiURI.buildURI(prizeCard, hand, maxCard)
 
-        HttpGet request = new HttpGet(uri)
+        def request = new HttpGet(uri)
 
-        try (CloseableHttpClient httpClient = HttpClients.createDefault()
-             CloseableHttpResponse response = httpClient.execute(request)) {
+        try (def httpClient = HttpClients.createDefault()
+             def response = httpClient.execute(request)) {
 
-            HttpEntity entity = response.getEntity()
-            if (entity != null) {
+            def entity = response.entity
+            if (entity) {
                 String resultStr = EntityUtils.toString(entity)
-                System.out.println("TRACER api remote: " + resultStr)
+                println("TRACER api remote: " + resultStr)
                 card = getCard(resultStr)
             }
         }
